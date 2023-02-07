b63817188ef8c1cbb981aaacc7122e101d7c9263
yusaflamichhane <124620173+yusaflamichhane@users.noreply.github.com>
2023-02-07 10:56:42 -0500

    Add files via upload
    Assignment on Loop

---
+++ AssignmentonLoop.java
@@ -0,0 +1,11 @@
+public class AssignmentonLoop {
+    public static void main(String[] args) {
+        //Write a program to print all the EVEN numbers from 3 to 30
+
+                for (int i = 4; i <= 30; i += 2) {
+                    System.out.println("Even numbers from 3 to 30 :"+i);
+                }
+            }
+        }
+
+
---
+++ AssignmentonLoop2.java
@@ -0,0 +1,10 @@
+public class AssignmentonLoop2 {
+    public static void main(String[] args) {
+//Write a program to print all the ODD numbers from 2 to 20//
+
+                for (int i = 3; i <= 19; i += 2) {
+                    System.out.println("Odd numbers from 2 to 20 :"+i);
+                }
+            }
+        }
+
